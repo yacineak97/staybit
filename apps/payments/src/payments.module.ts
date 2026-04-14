c545b69e35cd3106e2c1bb744f11f329da44c0d7
Yacine Akli <yacine.akli97@gmail.com>
2026-04-14 19:53:42 +0200

    feat(notifications): add notifications service and emit email after payment

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
-import { LoggerModule } from '@app/common';
+import { LoggerModule, NOTIFICATIONS_SERVICE } from '@app/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
@@ -13,9 +13,24 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
         STRIPE_SECRET_KEY: Joi.string().required(),
+        NOTIFICATIONS_HOST: Joi.string().required(),
+        NOTIFICATIONS_PORT: Joi.number().required(),
       }),
     }),
     LoggerModule,
+    ClientsModule.registerAsync([
+      {
+        name: NOTIFICATIONS_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get('NOTIFICATIONS_HOST'),
+            port: configService.get('NOTIFICATIONS_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,25 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
 import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe: Stripe;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {
     this.stripe = new Stripe(
       this.configService.getOrThrow<string>('STRIPE_SECRET_KEY'),
     );
   }
 
-  async createCharge({ card, amount }: PaymentsCreateChargeDto) {
+  async createCharge({ card, amount, email }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       currency: 'usd',
       payment_method: 'pm_card_visa',
       payment_method_types: ['card'],
       confirm: true,
+    });
+
+    this.notificationsService.emit('notify_email', {
+      email,
+      text: `We have successfully received your payment of $${amount}. Thank you for your reservation.`,
     });
 
     return paymentIntent;
